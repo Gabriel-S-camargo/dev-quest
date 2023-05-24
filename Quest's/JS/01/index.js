c6198f07eadc9390fb4a744265c1c94538fdef1b
Gabriel Santos Camargo <gabrielsantoscamargo8@gmail.com>
2023-05-23 21:53:46 -0300

    js quest

---
+++ Quest's/JS/01/index.js
@@ -0,0 +1,5 @@
+const nomeUser = prompt('Digite seu nome')
+console.log(nomeUser)
+
+alert('Seja bem vindo(a), ' + nomeUser + '!')
+
---
+++ Quest's/JS/02/index.js
@@ -0,0 +1,8 @@
+const boletoPago = false
+
+if (boletoPago === true){
+    alert('Boleto pago')
+}else if (boletoPago === false){
+    alert('Boleto não pago')
+} 
+
