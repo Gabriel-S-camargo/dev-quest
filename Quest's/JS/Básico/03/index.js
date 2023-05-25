6ed0a7b41abe0d2bc9fa4c2c2c5087e085cc0b77
Gabriel Santos Camargo <gabrielsantoscamargo8@gmail.com>
2023-05-24 21:13:13 -0300

    Atividades js

---
+++ Quest's/JS/Básico/03/index.js
@@ -0,0 +1,31 @@
+const confirm = document.getElementById("confirm");
+const deny = document.getElementById("deny");
+const header = document.getElementById("header"); 
+const retur = document.getElementById("return")
+
+// confirm.addEventListener("click", function(e){
+//     alert('Ótimo! Nós temos as melhores camas da região')
+// })
+
+// deny.addEventListener("click", function(e){
+//     alert('Que pena você parecia mais legal')
+// }) 
+
+function changecontent(){
+    header.innerHTML = '<p>Ótimo! Nós temos as melhores camas da região</p>'
+}
+
+function negativechangecontent(){
+    header.innerHTML = '<p>Que pena! Você parecia ser uma pessoa mais legal</p>'
+} 
+
+function returncontent(){
+    header.innerHTML = '<p>Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?</p>'
+}
+
+confirm.addEventListener("click", changecontent);
+
+deny.addEventListener("click", negativechangecontent);
+
+retur.addEventListener("click", returncontent );
+
