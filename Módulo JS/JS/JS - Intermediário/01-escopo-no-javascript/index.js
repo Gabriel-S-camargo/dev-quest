0829cefbf2529c57e804ffa1efed611d4c8934d3
Gabriel Santos Camargo <gabrielsantoscamargo8@gmail.com>
2023-05-30 20:42:40 -0300

    some changes in js

---
+++ Módulo JS/JS/JS - Intermediário/01-escopo-no-javascript/index.js
@@ -0,0 +1,13 @@
+// if(true){
+//     const mensagem = 'olá'
+//     console.log(mensagem)
+// }
+
+// console.log(mensagem)
+
+{
+    var mensagem = 'olá'
+    console.log(mensagem)
+}
+
+console.log(mensagem)
--- Quest's/JS/Básico/03/index.js
+++ Quest's/JS/Básico/03/index.js
@@ -1,33 +1,33 @@
-// const confirm = document.getElementById("confirm");
-// const deny = document.getElementById("deny");
-// const header = document.getElementById("header");
-// const retur = document.getElementById("return")
+const confirm = document.getElementById("confirm");
+const deny = document.getElementById("deny");
+const header = document.getElementById("header");
+const retur = document.getElementById("return")
 
-// confirm.addEventListener("click", function(e){
-//      alert('Ótimo! Nós temos as melhores camas da região')
-// })
+confirm.addEventListener("click", function(e){
+     alert('Ótimo! Nós temos as melhores camas da região')
+})
 
-// deny.addEventListener("click", function(e){
-//      alert('Que pena você parecia mais legal')
-// })
+deny.addEventListener("click", function(e){
+     alert('Que pena você parecia mais legal')
+})
 
-// function changecontent(){
-//     header.innerHTML = '<p>Ótimo! Nós temos as melhores camas da região</p>'
-// }
+function changecontent(){
+    header.innerHTML = '<p>Ótimo! Nós temos as melhores camas da região</p>'
+}
 
-// function negativechangecontent(){
-//     header.innerHTML = '<p>Que pena! Você parecia ser uma pessoa mais legal</p>'
-// }
+function negativechangecontent(){
+    header.innerHTML = '<p>Que pena! Você parecia ser uma pessoa mais legal</p>'
+}
 
-// function returncontent(){
-//     header.innerHTML = '<p>Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?</p>'
-// }
+function returncontent(){
+    header.innerHTML = '<p>Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?</p>'
+}
 
-// confirm.addEventListener("click", changecontent);
+confirm.addEventListener("click", changecontent);
 
-// deny.addEventListener("click", negativechangecontent);
+deny.addEventListener("click", negativechangecontent);
 
-// retur.addEventListener("click", returncontent );
+retur.addEventListener("click", returncontent );
 
 const vaiSeHospedar = confirm ('Seja bem vindo(a) aventureiro(a)! Gostaria de pagar R$ 50,00 apra passar a noite na nossa hospedagem ?')
 
