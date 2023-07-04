b3f129a90158ec3dc1b223ddec4cdcdd149d41c9
Gabriel Santos Camargo <gabrielsantoscamargo8@gmail.com>
2023-07-04 12:35:46 -0300

    Arrays multidimensionais e operações com arrays

---
+++ Módulo JS/JS/JS - Intermediário/08 - Trabalahndo com arrays/index.js
@@ -0,0 +1,13 @@
+let frutas = ['banana', 'maça', 'laranja', 'pêra']
+let frutasTamanho = frutas.length
+
+// for(let i = 0; i < frutasTamanho; i++){
+//     console.log('fruta: ' + frutas[i])
+
+// }
+
+// frutas.forEach(function(item,){
+//     console.log('fruta: ' + item)
+// })
+
+frutas.push('manga')
