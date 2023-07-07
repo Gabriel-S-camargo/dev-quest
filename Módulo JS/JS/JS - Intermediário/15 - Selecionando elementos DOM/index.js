7ebf07fbca9d1eee180b4a92877b073f9ef18b1b
Gabriel Santos Camargo <gabrielsantoscamargo8@gmail.com>
2023-07-07 17:57:05 -0300

    buscadores

--- Módulo JS/JS/JS - Intermediário/15 - Selecionando elementos DOM/index.js
+++ Módulo JS/JS/JS - Intermediário/15 - Selecionando elementos DOM/index.js
@@ -1,4 +1,13 @@
-let p = document.getElementById('paragrafo1')
+// let p = document.getElementById('paragrafo1')
 
-console.log (p)
+// console.log (p)
 
+let inputsPorClasse = document.getElementsByClassName('tag-de-input')
+
+console.log (inputsPorClasse[0])
+
+let inputPorTag = document.getElementsByTagName('h1')
+
+console.log (inputPorTag)
+
+let imputPorNome = document.querySelector("input [name='email']")
